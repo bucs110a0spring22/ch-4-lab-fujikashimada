091492b2ea0e93610b1a3f9195c221c808a44fc7
Fujiko Kashimada <98532804+fujikashimada@users.noreply.github.com>
2022-03-09 04:46:49 +0000

    completed lab

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import turtle
 
 ########### Your Code here ##############
@@ -5,13 +6,42 @@ import turtle
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def drawSineCurve(turtle=None):
+  for x_value in range(720):
+    y_value = math.sin(math.radians(x_value-360))
+    turtle.goto(x_value-360, y_value)
+    turtle.down()
+  turtle.up()
 
+def drawCosineCurve(turtle=None):
+  for x_value in range(720):
+    y_value = math.cos(math.radians(x_value-360))
+    turtle.goto(x_value-360, y_value) 
+    turtle.down()
+  turtle.up()
 
+def drawTangentCurve(turtle=None):
+  for x_value in range(720):
+    if (x_value % 90 != 0):
+      y_value = math.tan(math.radians(x_value-360))
+      turtle.goto(x_value-360, y_value)
+      turtle.down()
+    else:
+      turtle.up()
+  turtle.up()
 
+def setupWindow(wn=None):
+  wn.setworldcoordinates(-360, -2, 360, 2)
 
-
-
-
+def setupAxis(turtle=None):
+  turtle.goto(0, -2)
+  turtle.down()
+  turtle.goto(0, 2)
+  turtle.up()
+  turtle.goto(360, 0)
+  turtle.down()
+  turtle.goto(-360, 0)
+  turtle.up()
 
 ##########  Do Not Alter Any Code Past Here ########
 def main():
